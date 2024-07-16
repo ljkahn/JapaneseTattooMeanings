920f434465e5dc8f85d8c98f976be075a347fc50
Lia Kahn <lia.j.kahn@gmail.com>
2024-07-16 14:37:19 -0600

    deleted weird nested repo??

--- japanese-tattoo-meanings/class-repo-pristine/DU-VIRT-FSF-FT-08-2023-U-LOLC/20-React/01-Activities/16-Stu_React-Forms/Solved/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import Form from './components/Form/index';
-
-function App() {
-  return <Form />;
-}
-
-export default App;
--- japanese-tattoo-meanings/class-repo-pristine/DU-VIRT-FSF-FT-08-2023-U-LOLC/20-React/01-Activities/18-Stu_Hooks-useEffect/Solved/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import Thermostat from './components/Thermostat';
-
-function App() {
-  return <Thermostat />;
-}
-
-export default App;
